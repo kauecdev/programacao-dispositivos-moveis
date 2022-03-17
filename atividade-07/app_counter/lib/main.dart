69d8415ee8100bdaac4094c1d664acd5b83d3a9a
Kauê Cavalcante <61801350+kauecdev@users.noreply.github.com>
2022-03-16 23:43:54 -0300

    Add files via upload

---
+++ atividade-07/app_counter/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: const Text("Counteer"),
+          backgroundColor: Colors.blue,
+        ),
+        body: MyCounterPage(),
+      ),
+    )
+  );
+}
+
+class MyCounterPage extends StatefulWidget {
+  @override
+  State<MyCounterPage> createState() => _MyCounterPage();
+}
+
+class _MyCounterPage extends State<MyCounterPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      if (_counter != 0) {
+        _counter--;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+        ],
+      )
+    );
+  }
+}
